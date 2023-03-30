bc494047c0a929113b1f4f4cdd2901c0bd2a1028
Enrique Comesaña <99300789+k1k3cb@users.noreply.github.com>
2023-03-30 23:23:53 +0200

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+
+export default defineConfig({
+  plugins: [ViteMinifyPlugin({}), ViteImageOptimizer({})],
+  base: '',
+  root: 'src',
+  build: {
+    assetsDir: 'assets',
+    outDir: '../docs'
+  }
+});
+
+
